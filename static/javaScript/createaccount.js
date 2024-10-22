7440cc8996da9887cc2eaf40ede1304aff05eee1
sagir koki <KokiS@cardiff.ac.uk>
2024-10-22 20:44:18 +0100

    flask for the create acoount page and updates on the javascript

--- static/javaScript/createaccount.js
+++ static/javaScript/createaccount.js
@@ -1,9 +1,10 @@
-document.getElementById('create_account').addEventListener('submit',function(event) {
+var form = document.getElementById('create_account')
+form.addEventListener('submit',function(event) {
     event.preventDefault();
-    var name = document.getElementById('name').Value;
-    var email = document.getElementById('email').Value;
-    var password = document.getElementById('password').value;
-    var phone = document.getElementById('phone').value;
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var phone = document.getElementById("phone").value;
 
     let isValid = true;
 
@@ -17,29 +18,30 @@ document.getElementById('create_account').addEventListener('submit',function(eve
     if(!nameRegex.test(name)){
         displayErrorMessage("name","please enter a valid name(aplhabets only");
         isValid = false;
+        return;
     }
     if(!emailRegex.test(email)){
         displayErrorMessage("email","please enter a valid email address");
         isValid = false;
+        return;
     }
     if(!passwordRegex.test(password)){
         displayErrorMessage("password","please enter a vlid password (alphabets and numbers only)");
         isValid = false;
+        return;
     }
     if(!phoneRegex.test(phone)){
         displayErrorMessage("phone","please enter a valid phone number 11 digit only" );
-        isValid=true;
+        isValid=false;
+        return;
     }
 
-    if(isValid){
-        alert('form is valid submitting');
-        this.submit();
-    }
-    function displayErrorMessage(inputId, message){
+   
+    function displayErrorMessage(inputId, message ){
     clearErrorMessages();
     
     let inputElement = document.getElementById(inputId);
-    let errorElement = document.createElement("section");
+    let errorElement = document.createElement("div");
     errorElement.className = "error";
     errorElement.innerHTML = message;
     inputElement.parentElement.insertBefore(errorElement,inputElement.nextSibling);
@@ -50,9 +52,12 @@ document.getElementById('create_account').addEventListener('submit',function(eve
             errors[0].parentNode.removeChild(errors[0]);
         }
     }   
-    
+    if(isValid){
+        HTMLFormElement.prototype.submit.call(form)
+        
+    }
 
 
 
 
-});
+})
