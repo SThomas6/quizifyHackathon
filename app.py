80f25f471eaf417afd15c8cae80724df60d64fcd
Ben Peck <peckbs@cardiff.ac.uk>
2024-10-24 16:29:55 +0100

    Updates the styles to match the other webpages

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, request, render_template, request, redirect
 
 app=Flask (__name__)
-@app.route("/login_page", methods=['POST','GET'])    
-def login_page():
+@app.route("/loginPage", methods=['POST','GET'])    
+def loginPage():
     if request.method =='GET':
 
 
 
-        return render_template("LogIn.html")
+        return render_template("login.html")
 
     if request.method =='POST':
         data = request.form
